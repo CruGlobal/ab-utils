ded7484cdeb40df5e9494c85d9dfb1e6deca0623
Johnny Hausman <johnny@hausmanfamily.com>
2019-02-05 16:35:48 +0700

    first commit

---
+++ ab-utils.js
@@ -0,0 +1,7 @@
+// ab-utils
+// a set of common utilities shared by each of our microsservices
+//
+var config = require('./utils/config.js');
+module.exports = {
+	config
+}
---
+++ utils/config.js
@@ -0,0 +1,7 @@
+//
+// config
+// read in this service's config file, merge it with the settings in the common
+// local.js file, and return those values.
+module.exports = (baseFile) => {
+	
+}
