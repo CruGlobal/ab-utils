cc133ba27e5ab23a51a5de54f4e4b5bed1fcb88b
Johnny Hausman <johnny@hausmanfamily.com>
2020-01-03 12:58:24 +0700

    [add] reqAB : initial parameter validation capability.

--- utils/reqAB.js
+++ utils/reqAB.js
@@ -3,25 +3,43 @@
  * prepare a default set of data/utilities for our api request.
  */
 const shortid = require("shortid");
-module.exports = function() {
-    return {
-        jobID: shortid.generate(),
-        log: function(...allArgs) {
-            var args = [];
-            allArgs.forEach((a) => {
-                args.push(JSON.stringify(a));
-            });
-            console.log(`${this.jobID}::${args.join(" ")}`);
-        },
-        toParam: function(key, data) {
-            data = data || {};
-            return {
-                type: key,
-                param: {
-                    jobID: this.jobID,
-                    data
-                }
-            };
-        }
-    };
+module.exports = function(req, res) {
+   return {
+      jobID: shortid.generate(),
+      log: function(...allArgs) {
+         var args = [];
+         allArgs.forEach((a) => {
+            args.push(JSON.stringify(a));
+         });
+         console.log(`${this.jobID}::${args.join(" ")}`);
+      },
+      toParam: function(key, data) {
+         data = data || {};
+         return {
+            type: key,
+            param: {
+               jobID: this.jobID,
+               data
+            }
+         };
+      },
+      validateParameters: function(description, autoRespond) {
+         description = description || {};
+         if (typeof autoRespond == "undefined") {
+            autoRespond = true;
+         }
+
+         /*
+          * description = {
+          *   "field" : { type: "type", required: [true|false] }
+          * }
+          */
+
+         return this.__validationErrors.length == 0;
+      },
+      errorValidation: function() {},
+      __req: req,
+      __res: res,
+      __validationErrors: []
+   };
 };
